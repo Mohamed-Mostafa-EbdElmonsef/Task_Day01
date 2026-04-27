55ea203645640e9678702f8e763cb6d2b2df326c
Dell <Dell@DESKTOP-IAKR6TS>
2026-04-27 17:03:50 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Islam Carpet Cleaning Service");
+            Console.WriteLine("We offer Carpet Cleaning Service : 25$ for small carpets and 35$ for large carpet");
+            Console.Write("Enter the number of small carpets :");
+            int SmallCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of large carpets :");
+            int LargeCarpets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The price for the small carpet is 25$ ,for large carpet is 35$ and there is Taxes 6.6$");
+            Console.WriteLine($"The cost without taxes = {(SmallCarpets*25)+(LargeCarpets*35)}");
+            Console.WriteLine($"The cost with taxes = {(SmallCarpets*25)+(LargeCarpets*35)+6.6}");
+
+        }
+    }
+}
